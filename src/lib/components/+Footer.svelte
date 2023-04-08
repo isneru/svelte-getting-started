05baccf54f45825f87eb7d766284ea7a13885fcd
isneru <diogoclanogueira@gmail.com>
2023-04-08 05:56:34 +0100

    footer, icons

---
+++ src/lib/components/+Footer.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Github from "./icons/+Github.svelte"
+</script>
+
+<footer class="absolute bottom-0 left-0 right-0 flex h-16 items-center justify-center">
+	<a
+		href="https://github.com/isneru/svelte-getting-started"
+		target="_blank"
+		rel="noopener noreferrer"><Github size={24} /></a>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+	import Footer from "$lib/components/+Footer.svelte"
 	import "../styles/globals.css"
 </script>
 
 <slot />
+<Footer />
 
 <style lang="postcss">
 	:global(body) {
