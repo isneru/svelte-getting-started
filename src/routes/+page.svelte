bd30c1fe591217738630761fa80a38f91c15fa00
isneru <diogoclanogueira@gmail.com>
2023-04-08 09:32:56 +0100

    lol.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import Todo from "$lib/components/+Todo.svelte"
-	import Spinner from "$lib/components/icons/+Spinner.svelte"
 	import { addTodo, todos } from "$lib/stores/todos"
 	import { onDestroy, onMount } from "svelte"
 	import { get } from "svelte/store"
@@ -17,14 +16,10 @@
 
 <div class="container mx-auto flex h-screen flex-col items-center justify-center">
 	<div class="flex flex-col gap-3 font-semibold">
-		{#if !$todos.length}
-			<span class="flex items-center gap-5 text-xl font-bold"><Spinner /> Loading</span>
-		{:else}
-			{#each $todos as todo}
-				<Todo {todo} />
-			{/each}
-			<button class="w-full rounded bg-emerald-500 px-3 py-2 text-black" on:click={addTodo}
-				>Add todo</button>
-		{/if}
+		{#each $todos as todo}
+			<Todo {todo} />
+		{/each}
+		<button class="w-full rounded bg-emerald-500 px-3 py-2 text-black" on:click={addTodo}
+			>Add todo</button>
 	</div>
 </div>
