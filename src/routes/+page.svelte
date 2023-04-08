7f22d30318d115ed4960ab99435aa05f2866e5be
isneru <diogoclanogueira@gmail.com>
2023-04-08 05:40:08 +0100

    Add todo button inline padding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 		{#each $todos as todo}
 			<Todo {todo} />
 		{/each}
-		<button class="w-full rounded bg-emerald-500 py-2 text-black" on:click={addTodo}
+		<button class="w-full rounded bg-emerald-500 px-3 py-2 text-black" on:click={addTodo}
 			>Add todo</button>
 	</div>
 </div>
